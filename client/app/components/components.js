77b95471b7a96f4fed7c7a32f4a109776fb7214c
Matt Hsiung <xung@musician.com>
2016-07-21 22:32:10 -0400

    added secure states

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,13 +1,11 @@
 import angular from 'angular';
 import Home from './home/home';
 import Login from './login/login';
-import Members from './members/members';
 import Api from './api/api';
 
 let componentModule = angular.module('app.components', [
   Home.name,
   Login.name,
-  Members.name,
   Api.name
 ]);
 
